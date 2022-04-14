19ab9e522ea8c3971f5571c68075dbab43212eee
Daniel Fernandez Cacho <Dani.Fernandezcacho@gmail.com>
2022-04-14 10:21:12 +0200

    Atributos / Set get

--- UD9-Ejercicio-3/src/Libro.java
+++ UD9-Ejercicio-3/src/Libro.java
@@ -1,4 +1,37 @@
 
 public class Libro {
 
+	private String isbn;
+	private String titulos;
+	private String autor;
+	private int nPaginas;
+	
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitulos() {
+		return titulos;
+	}
+	public void setTitulos(String titulos) {
+		this.titulos = titulos;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getnPaginas() {
+		return nPaginas;
+	}
+	public void setnPaginas(int nPaginas) {
+		this.nPaginas = nPaginas;
+	}
+	
+	
+	
 }
