ae4ef3ffca1ed9758b74e822805cbabcc0c7c9c8
Daniel Fernandez Cacho <Dani.Fernandezcacho@gmail.com>
2022-04-14 10:32:17 +0200

    Main i compare paginas

---
+++ UD9-Ejercicio-3/src/Ej3.java
@@ -0,0 +1,18 @@
+
+public class Ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void compararPaginas(Libro a, Libro b) {
+		
+		if (a.getnPaginas() > b.getnPaginas()) {
+			System.out.println(a.getTitulos() + " tiene mas paginas que " + b.getTitulos());
+		}
+		else {
+		System.out.println(b.getTitulos() + " tiene mas paginas que " + a.getTitulos());
+		}
+	}
+}
